9873cbfd16e9c388261ab53295203fb3bd14ba84
mattyatea <mattyacocacora0@gmail.com>
2025-04-17 10:37:32 +0900

    リファクタリング 共通化できる処理を共通化 明示的にnodeを使用するように

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { EsaClient } from "esa-api-client"
+import { EsaClient, Post } from "esa-api-client"
 import { z } from "zod";
+
 if (!process.env.ESA_API_TOKEN || !process.env.ESA_TEAM_NAME) {
     console.error("Please set ESA_API_TOKEN and ESA_TEAM_NAME environment variables.");
     process.exit(1);
@@ -11,16 +12,24 @@ const esa = new EsaClient({
     token: process.env.ESA_API_TOKEN,
     teamName: process.env.ESA_TEAM_NAME, 
 });
- 
-// Create server instance
+
 const server = new McpServer({
     name: "esa",
     version: "1.0.0",
-    capabilities: {
-        resources: {},
-        tools: {},
-    },
 });
+
+function formatPost(post: Post): {type: "text", text: string} {
+    return {
+        type: "text",
+        text: `${post.name} (${post.full_name}) \n${post.wip ? "WIP" : "Published"}\n${post.body_md}`,
+    };
+}
+
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Esa MCP Server running on stdio");
+}
 
 server.tool(
     "search_esa_posts",
@@ -29,10 +38,9 @@ server.tool(
     async ({searchParams}) => {
         const posts = await esa.getPosts({q: encodeURI(searchParams.replace(' ', '+'))})
         return {
-            content: posts.posts.map((post) => ({
-                type: "text",
-                text: `${post.name} (${post.full_name}) \n${post.wip ? "WIP" : "Published"}\n${post.body_md}`,
-            })),
+            content: posts.posts.map((post) => 
+                formatPost(post)
+            ),
         };
     },
 );
@@ -42,14 +50,11 @@ server.tool("get_latest_esa_posts", "Get Latest Esa Posts", {}, async () => {
         page: 1,
         per_page: 5,
     });
-    
+
     return {
-        content: posts.posts.map((post) => {
-            return {
-                type: "text",
-                text: `${post.name} (${post.full_name}) \n${post.wip ? "WIP" : "Published"}\n${post.body_md}`,
-            };
-        })
+        content: posts.posts.map((post) =>
+            formatPost(post)
+        ),
     };
 })
 
@@ -73,12 +78,6 @@ server.tool("create_esa_post", "Create a new post in esa", {title: z.string().de
         }],
     };
 })
-
-async function main() {
-    const transport = new StdioServerTransport();
-    await server.connect(transport);
-    console.error("Esa MCP Server running on stdio");
-}
 
 main().catch((error) => {
     console.error("Fatal error in main():", error);
